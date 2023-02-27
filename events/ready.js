af87ebe5e48103214fc119d2d1f3564d468af381
Nobita18 <93038956+Nobitaisbest@users.noreply.github.com>
2023-02-27 17:55:29 +0530

    Delete events directory

--- events/ready.js
+++
@@ -1,7 +0,0 @@
-const { EmbedBuilder, Collection, PermissionsBitField } = require('discord.js')
-const ms = require('ms');
-const client = require('..');
-const config = require('../config.json');
-client.on('ready',mes =>{
-    console.log(`LOGGED IN AS ${client.user.tag}`)
-})
