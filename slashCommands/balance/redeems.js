5dc15a02885a3e3c93da4b8887c720911117d7a4
Nobita18 <93038956+Nobitaisbest@users.noreply.github.com>
2023-02-28 17:11:14 +0000

    update redeems.js

--- slashCommands/balance/redeems.js
+++ slashCommands/balance/redeems.js
@@ -13,8 +13,9 @@ module.exports = {
     let embed = new EmbedBuilder()
       .setColor('NotQuiteBlack')
       .setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL() })
-      .setFields({ name: "Redeems", value: `**${parseInt(redeems)}**` , inline: true },
+      .setFields({ name: "Your Redeems", value: `**${parseInt(redeems)}**` , inline: true },
                 { name: "Shards", value: `**${parseInt(shards)}**` , inline: true })
+      .setDescription('You can use redeems to receive any character of your choice. You can receive redeems by purchasing them with shards or through voting rewards. \n Use a redeem [/redeemspawn] to spawn a character of your choice in the current channel (careful, if something else spawns, itll be overridden).')
       .setThumbnail(interaction.user.displayAvatarURL())
       .setFooter({ text: "Bot developed by 《☆ Nobita ☆》#8773 and Naruto Uzumaki#1353" })
       .setTimestamp()
