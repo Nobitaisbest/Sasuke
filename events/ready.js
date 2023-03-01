94ef21a078fb8d0d441befa16abdd74b1108431d
Nobita18 <93038956+Nobitaisbest@users.noreply.github.com>
2023-03-01 07:53:25 +0000

    status update

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,27 @@
-const { EmbedBuilder, Collection, PermissionsBitField } = require('discord.js')
-const ms = require('ms');
+const { ActivityType } = require('discord.js');
 const client = require('..');
-const config = require('../config.json');
-client.on('ready',mes =>{
-    console.log(`LOGGED IN AS ${client.user.tag}`)
-})
+const chalk = require('chalk');
+
+client.on("ready", () => {
+	const activities = [
+        {name: `One piece` , type: ActivityType.Streaming , url: `https://www.twitch.tv/discord`}
+	];
+	const status = [
+		'online',
+		'dnd',
+	];
+	let i = 0;
+	setInterval(() => {
+		if(i >= activities.length) i = 0
+		client.user.setActivity(activities[i])
+		i++;
+	}, 5000);
+
+	let s = 0;
+	setInterval(() => {
+		if(s >= activities.length) s = 0
+		client.user.setStatus(status[s])
+		s++;
+	}, 30000);
+	console.log(chalk.red(`Logged in as ${client.user.tag}!`))
+});
